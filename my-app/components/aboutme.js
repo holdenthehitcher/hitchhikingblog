927d1713610844f48fc4affe4841e4094eec8ce4
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-03 20:59:07 -1000

    changed header, added <AboutMe/ >

---
+++ my-app/components/aboutme.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+export default function AboutMe() {
+    return (
+        <div>
+            <img alt='About Me Image' href=''></img>
+            <p>This is my about me section</p>
+        </div>
+    )
+}

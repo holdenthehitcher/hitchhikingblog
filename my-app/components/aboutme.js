da5ec313849a748e41b9ad17a7a2ed9d33f9576b
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-03 21:11:46 -1000

    installed bootstrap with _app.js importing it

--- my-app/components/aboutme.js
+++ my-app/components/aboutme.js
@@ -1,10 +1,11 @@
-import { Component } from 'react'
+import { Component } from "react";
 
 export default function AboutMe() {
-    return (
-        <div>
-            <img alt='About Me Image' href=''></img>
-            <p>This is my about me section</p>
-        </div>
-    )
+  return (
+    <div>
+      <img alt="About Me Image" href=""></img>
+      <p>This is my about me section</p>
+      <button className="btn btn-lg text-danger bg-success">This works!</button>
+    </div>
+  );
 }

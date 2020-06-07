9ee484118c553da710ed923674d1a832ee5f5875
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-07 09:29:55 -1000

    moved styles from home to css/styles.css

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import "bootstrap-social/bootstrap-social.css";
+import "../css/styles.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

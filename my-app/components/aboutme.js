141111624f6ffc84ad1e8468779f5449a9c82fa8
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-07 22:58:18 -1000

    moved blogmaterials to public

--- my-app/components/aboutme.js
+++ my-app/components/aboutme.js
@@ -4,8 +4,31 @@ export default function AboutMe() {
   return (
     <div className="container">
       <div className="row">
-        <p>This is my about me section</p>
-        <button className="btn btn-lg text-danger bg-success">This works!</button>
+        <div className="col-3">
+          <img src="blogmaterials/about/about.JPG" id="aboutImg"></img>
+        </div>
+        <div className="col-9">
+          <h3>About Holden the Hitcher</h3>
+          <p>
+            Three years following my return to the States… I spent time making blog posts about my travels… without real
+            intent to implement them professionally. Now, after increasing my knowledge of website development, my
+            experiences can now take on a life of their own. You won’t believe the amount of adventure that can fit
+            within two years; nor will you see better examples the giving nature of people to those who find themself in
+            a less than ideal situation.
+            <br/>
+            Disclaimer: This is not your Average ‘travel blog’!
+            <br/>
+            This follows my journey
+            through 20 countries in Europe with nothing but a backpack and intentions, learning about different
+            cultures, the people embodying them, and myself in the process. Stories also include volunteering on farms,
+            homes, and hostels, along with some culinary facts and insights that may be foreign… or really hit close
+            home with you. There is a favorite gear page for some of the tools I used with wild and urban camping when I
+            wanted a place to sleep in more novel locations. My site aims to be ADA compliant using keyboard functions
+            only. Please refer to
+            <u>this</u> to see which keys you need to use to access all the content on this site. Thanks for stopping by
+            and I hope you learn something new today in an interesting way, from the experiences I’ve lived through.
+          </p>
+        </div>
       </div>
     </div>
   );

a8da9d51e9a15e948967fda690e7af124503b054
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-08 18:32:37 -1000

    added structure to InspirationalQuote

--- my-app/components/InspirationalQuote.js
+++ my-app/components/InspirationalQuote.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function InspirationalQuote() {
   return (
     <div className="row">
-      <div className="col-7"></div>
-      <div className="col"></div>
+      <div className="col-7">
+        <Card className="carouselCard">
+            <Card.Body>
+                
+            </Card.Body>
+        </Card>
+      </div>
+      <div className="col">
+        <Card className="carouselCard">
+            <Card.Body>
+                
+            </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }

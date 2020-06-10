b2cf1ea89fed5334782bc3e7ffba782e487d06ea
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-10 13:11:53 -1000

    minor text changes to ArticlesFeatured

--- my-app/components/RandomArticle.js
+++ my-app/components/RandomArticle.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-
 export default function RandomArticle() {
   return (
     <div>
@@ -9,8 +8,8 @@ export default function RandomArticle() {
         <Card.Header>Random Article Generator</Card.Header>
         <Card.Body>
           <Card.Title>Needing Some Inspiration?</Card.Title>
-          <Card.Text>Check out our a random article picker to add novelty to your day!</Card.Text>
-          <button className="primary">Choose Story</button>
+          <Card.Text>Check out our a random article chooser to add novelty to your day!</Card.Text>
+          <button className="primary">Pick A Story</button>
         </Card.Body>
         <Card.Footer className="text-muted">2 days ago</Card.Footer>
       </Card>

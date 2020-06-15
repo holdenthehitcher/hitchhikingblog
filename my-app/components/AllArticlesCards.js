c33d2bfd511c3d42484d6cda20befdd0f93edccc
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-15 12:40:40 -1000

    put title at top of cards and changed name for AllArticlesCards

--- my-app/components/AllArticlesCards.js
+++ my-app/components/AllArticlesCards.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-export default function AllArticles({ blogposts }) {
+export default function AllArticlesCards({ blogposts }) {
   return (
     <>
       {blogposts.map(({ thumbnail }, i) => (
         <Card key={i}>
+          <Card.Title>{thumbnail.title}</Card.Title>
           <Card.Img variant="top" src={thumbnail.image} />
           <Card.Body>
-            <Card.Title>{thumbnail.title}</Card.Title>
             <Card.Text>{thumbnail.description}</Card.Text>
           </Card.Body>
         </Card>
--- my-app/pages/articles.js
+++ my-app/pages/articles.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 import ArticlesFeatured from "../components/ArticlesFeatured";
 import RandomArticle from "../components/RandomArticle";
 import SortingButtons from "../components/SortingButtons";
-import AllArticles from "../components/AllArticles";
+import AllArticlesCards from "../components/AllArticlesCards";
 import ArticlesPaginate from "../components/ArticlesPaginate";
 
 import { ALLPOSTS } from "../shared/ALLPOSTS";
@@ -26,7 +26,7 @@ export default function Articles() {
           <RandomArticle />
           <SortingButtons />
           <CardColumns>
-            <AllArticles blogposts={blogposts} />
+            <AllArticlesCards blogposts={blogposts} />
           </CardColumns>
           <ArticlesPaginate />
         </div>

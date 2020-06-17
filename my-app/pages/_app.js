ecaf39b26e600f40de024feb3d918861bcba0de7
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-16 21:49:12 -1000

    created Fonts in styles fold. with links to fonts in Head of _app, only worked after rerunning dev

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -2,14 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import "bootstrap-social/bootstrap-social.css";
 import "../styles/styles.scss";
+import Fonts from "../styles/Fonts";
 
+import Head from "next/head";
 
 import "../styles/fonts.js";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-  <>
-    <Component {...pageProps} />;
-  </>
-  )
+    <>
+      <Head>
+        <Fonts />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- my-app/styles/fonts.js
+++ my-app/styles/fonts.js
@@ -1,6 +1,12 @@
-<>
-  <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet"></link>;
-  <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet"></link>
-  <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet"></link>
-  <link href="https://fonts.googleapis.com/css2?family=Bellota:wght@700&display=swap" rel="stylesheet"></link>
-</>;
+import React from "react";
+
+export default function Fonts() {
+  return (
+    <div>
+      <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet"></link>;
+      <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Bellota:wght@700&display=swap" rel="stylesheet"></link>
+    </div>
+  );
+}

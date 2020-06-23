e758e86873d96ec0f1e0132065394ea01c6255fe
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-22 23:16:04 -1000

    added classNames to allposts by category to change background color of cards in AllArticles

--- my-app/components/AllArticlesCards.js
+++ my-app/components/AllArticlesCards.js
@@ -5,16 +5,16 @@ import Link from "next/link";
 export default function AllArticlesCards({ blogposts }) {
   return (
     <>
-      {blogposts.map(({ thumbnail, pageLink = "/allstories" }, i) => (
-        <Card key={i} className="allCard">
+      {blogposts.map(({ thumbnail, pageLink = "/allstories", className }, i) => (
+        <Card key={i} className={`${className}`}>
           <Link href={pageLink}>
             <a>
-              <Card.Title className="quoteAuthor" style={{ textAlign: "center" }}>
+              <Card.Title className="allCardTitle" style={{ textAlign: "center" }}>
                 {thumbnail.title}
               </Card.Title>
-              <Card.Img variant="top" src={thumbnail.image} />
-              <Card.Body className="d-none ">
-                <Card.Text className="quoteText">{thumbnail.description}</Card.Text>
+              <Card.Img variant="top" className="allCardImage" src={thumbnail.image} />
+              <Card.Body className="">
+                <Card.Text className="quoteText allCardText">{thumbnail.description}</Card.Text>
               </Card.Body>
             </a>
           </Link>

5e160e03e194f7e958b47a291e64547b6e4e95d1
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-28 22:57:01 -1000

    styled allarticlescards a lot

--- my-app/components/AllArticlesCards.js
+++ my-app/components/AllArticlesCards.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import CardColumns from "react-bootstrap/CardColumns";
+import ListGroup from "react-bootstrap/ListGroup";
+
 import Link from "next/link";
 
 import { Fade, Stagger } from "react-animation-components";
@@ -20,20 +22,22 @@ const AllArticlesCards = ({ blogposts, currentPage }) => {
       <Stagger in>
         {blogposts.map(({ thumbnail, pageLink = "/allstories", className, date, location }, i) => (
           <Fade key={i}>
-            <Card className={`${className}`}>
-              <Card.Title className="allCardTitle" style={{ textAlign: "center" }}>
+            <Card className={`${className} allCard`}>
+              <Card.Title className="allCardHeader" style={{ textAlign: "center" }}>
                 {thumbnail.title}
               </Card.Title>
 
               <Link href={pageLink}>
                 <a>
-                  <Card.Img variant="top" className="allCardImage" src={thumbnail.image} />
+                  <Card.Img variant="top" className="allCardImg" src={thumbnail.image} />
                 </a>
               </Link>
-              <Card.Body className="">
-                <Card.Text className="quoteText allCardText">{thumbnail.description}</Card.Text>
-              </Card.Body>
               <Card.Body>
+                <ListGroup>
+                  <ListGroup.Item className="allCardTextBg">
+                    <Card.Text className="allCardText">{thumbnail.description}</Card.Text>
+                  </ListGroup.Item>
+                </ListGroup>
                 <Card.Text className="allCardLocation">{location}</Card.Text>
                 <Card.Text className="allCardDate">{dateString(date)}</Card.Text>
               </Card.Body>
--- my-app/components/ArticlesFeatured.js
+++ my-app/components/ArticlesFeatured.js
@@ -10,29 +10,33 @@ export default function ArticlesFeatured() {
       header: "Hitchhiking",
       image: "blogmaterials/categories/hitchhiking_category.JPG",
       link: "/hitchhiking",
+      catBgStyle: "hitchhikingCard",
     },
     {
       header: "Volunteering",
       image: "blogmaterials/categories/volunteering_category.JPG",
       link: "/volunteering",
+      catBgStyle: "volunteerCard",
     },
     {
       header: "Culinary",
       image: "blogmaterials/categories/culinary_category.JPG",
       link: "/culinary",
+      catBgStyle: "culinaryCard",
     },
     {
       header: "Gear",
       image: "/blogmaterials/gear/gear_category.JPG",
       link: "/gear",
+      catBgStyle: "gearCard",
     },
   ];
 
   return (
     <div className="row">
       <CardDeck className="featuredCatDeck">
-        {articles.map(({ header, image, link }, i) => (
-          <Card className="featuredCat" key={i}>
+        {articles.map(({ header, image, link, catBgStyle }, i) => (
+          <Card className={`featuredCat ${catBgStyle}`} key={i}>
             <Link href={link}>
               <a>
                 <Card.Header className="featuredCardHeader">{header}</Card.Header>

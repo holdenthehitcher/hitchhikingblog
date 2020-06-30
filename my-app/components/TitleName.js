a0f5a566b010131b99bf089bbf41f509e9a834a8
Holden Nakamura <holdenthehitcher@gmail.com>
2020-06-30 11:08:38 -1000

    added favicon and changed Title

--- my-app/components/TitleName.js
+++ my-app/components/TitleName.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 export default function TitleName() {
   return (
     <Head>
-      <title>HTH</title>
+      <title>Holden the Hitcher</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>

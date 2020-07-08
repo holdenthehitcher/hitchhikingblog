37c106811209e34a20c652f8f083953257ec843b
Holden Nakamura <holdenthehitcher@gmail.com>
2020-07-08 11:15:05 -1000

    changed ARticlesFeatured into class component

--- my-app/components/ArticlesFeatured.js
+++ my-app/components/ArticlesFeatured.js
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 
 import Link from "next/link";
 import { CSSTransitionGroup } from "react-transition-group";
 
-export default function ArticlesFeatured() {
-  const articles = [
-    {
-      header: "Hitchhiking",
-      image: "blogmaterials/categories/hitchhiking_category.JPG",
-      link: "/hitchhiking",
-      catColor: "hitchhikingCard",
-    },
-    {
-      header: "Volunteering",
-      image: "blogmaterials/categories/volunteering_category.JPG",
-      link: "/volunteering",
-      catColor: "volunteerCard",
-    },
-    {
-      header: "Culinary",
-      image: "blogmaterials/categories/culinary_category.JPG",
-      link: "/culinary",
-      catColor: "culinaryCard",
-    },
-    {
-      header: "Gear",
-      image: "/blogmaterials/gear/gear_category.JPG",
-      link: "/gear",
-      catColor: "gearCard",
-    },
-  ];
+export default class ArticlesFeatured extends Component {
+  constructor(props) {
+    super(props);
 
-  const featuredCards = articles.map(({ header, image, link, catColor }, i) => (
+    this.state = {
+      articles: [
+        {
+          header: "Hitchhiking",
+          image: "blogmaterials/categories/hitchhiking_category.JPG",
+          link: "/hitchhiking",
+          catColor: "hitchhikingCard",
+        },
+        {
+          header: "Volunteering",
+          image: "blogmaterials/categories/volunteering_category.JPG",
+          link: "/volunteering",
+          catColor: "volunteerCard",
+        },
+        {
+          header: "Culinary",
+          image: "blogmaterials/categories/culinary_category.JPG",
+          link: "/culinary",
+          catColor: "culinaryCard",
+        },
+        {
+          header: "Gear",
+          image: "/blogmaterials/gear/gear_category.JPG",
+          link: "/gear",
+          catColor: "gearCard",
+        },
+      ],
+    };
+  }
+
+  render() {
+    const featuredCards = this.state.articles.map(({ header, image, link, catColor }, i) => (
       <Card className={`featuredCatCard ${catColor}`} key={i}>
         <Link href={link}>
           <a>
@@ -43,21 +50,19 @@ export default function ArticlesFeatured() {
           </a>
         </Link>
       </Card>
-    ))
-
-  return (
-    <div className="row">
-      <CSSTransitionGroup
-        transitionName="example"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionEnter={false}
-        transitionLeave={false}
-      >
-        <CardDeck className="featuredCatDeck">
-          {featuredCards}
-        </CardDeck>
-      </CSSTransitionGroup>
-    </div>
-  );
+    ));
+    return (
+      <div className="row">
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <CardDeck className="featuredCatDeck">{featuredCards}</CardDeck>
+        </CSSTransitionGroup>
+      </div>
+    );
+  }
 }
